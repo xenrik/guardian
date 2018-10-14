901c11a08132d28e65df3db560cd21f730946913
xenrik <lee@xenrik.com>
2018-10-14 10:27:25 +0100

    Realised I was doing completely the wrong thing - changed to predict where the ship will be and then shoot there

---
+++ Assets/Scenes/ScratchPad/Scripts/PredictingTarget/TargetMover.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetMover : MonoBehaviour {
+
+    public float ThrustForce;
+
+    public Vector3 TorqueForce;
+    public float TorqueInterval;
+
+    private new Rigidbody rigidbody;
+    private float torqueImpulse;
+
+	private void Start () {
+        rigidbody = GetComponent<Rigidbody>();
+        torqueImpulse = TorqueInterval;
+	}
+	
+	private void FixedUpdate () {
+        rigidbody.AddForce(transform.forward * ThrustForce);
+        if (torqueImpulse <= 0) {
+            rigidbody.AddTorque(TorqueForce, ForceMode.Impulse);
+            torqueImpulse = TorqueInterval;
+        } else {
+            torqueImpulse -= Time.fixedDeltaTime;
+        }
+	}
+}
