4325948ab66e8136f148cca2df7c8d867b79a918
Lee <lee@xenrik.com>
2024-12-29 12:18:19 +0000

    Working on floating origin

--- scripts/extensions/NodeExtensions.cs
+++ scripts/extensions/NodeExtensions.cs
@@ -33,4 +33,20 @@ public static class NodeExtensions {
             }
         }
     }
+
+    /**
+     * Returns true if this node is parented by the given node. 
+     */
+    public static bool HasParent(this Node node, Node test) {
+        Node parent = node.GetParent();
+        while (parent != null) {
+            if (parent == test) {
+                return true;
+            }
+
+            parent = parent.GetParent();
+        }
+
+        return false;
+    }
 }
